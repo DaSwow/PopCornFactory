7aa4fa81847952e3d009ca29cd335c8c0e5961d7
DaSwow <carls09995@hotmail.com>
2022-02-28 01:24:38 -0700

    Update Catalogo.kt

--- app/src/main/java/chaira/carlos/popcornfactory/Catalogo.kt
+++ app/src/main/java/chaira/carlos/popcornfactory/Catalogo.kt
@@ -29,63 +29,17 @@ class Catalogo : AppCompatActivity() {
     }
 
     fun cargarPeliculas(){
-        peliculas.add(Pelicula("Big hero",R.drawable.bighero6,R.drawable.headerbighero6,"When a devastating event befalls the city of San Fransokyo and catapults Hiro into the\n" +
-                "midst of danger, he turns to Baymax and his close friends adrenaline junkie Go Go\n" +
-                "Tomago, neatnik Wasabi, chemistry whiz Honey Lemon and fanboy Fred. Determined to\n" +
-                "uncover the mystery, Hiro transforms his friends into a band of high-tech heroes called 'Big Hero 6'."))
-        peliculas.add(Pelicula("1917",R.drawable.mil,R.drawable.milheader,"British trenches somewhere in France. World war has been going on for the third year,\n" +
-                "heroic illusions have dissipated; general mood - boredom and fatigue. Stuff the belly,\n" +
-                "sleep, return home to Christmas Eve. On another quiet day, when nothing happens, two\n" +
-                "young soldiers, Blake and Schofield, are summoned to the general, who instructs them to\n" +
-                "send an important message to Colonel MacKenzie in the Second Devonshire Battalion,\n" +
-                "whose telephone connection was cut off by the enemy."))
-        peliculas.add(Pelicula("Leap Year",R.drawable.leapyear,R.drawable.leapyearheader,"A woman who has an elaborate scheme to propose to her boyfriend on Leap Day, an Irish\n" +
-                "tradition which occurs every time the date February 29 rolls around, faces a major setback\n" +
-                "when bad weather threatens to derail her planned trip to Dublin. With the help of an\n" +
-                "innkeeper, however, her cross-country odyssey just might result in her getting engaged."))
-        peliculas.add(Pelicula("Men in black",R.drawable.mib,R.drawable.mibheader,"Based off of the comic book. Unbeknownst to other people, there is a private agency code\n" +
-                "named MiB. This agency is some kind of extra terrestrial surveillance corporation. Then,\n" +
-                "one of the agency&#39;s finest men only going by the name &quot;K&quot; (Tommy Lee Jones) , is\n" +
-                "recruiting for a new addition to the agency. He has chosen James Edwards (Will Smith) of\n" +
-                "the N.Y.P.D. Then, one day, a flying saucer crashes into Earth. This was an alien a part of\n" +
-                "the &quot;Bug&quot; race. He takes the body of a farmer (Vincent D&#39;Onofrio) and heads to New York.\n" +
-                "He is searching for a super energy source called &quot;The Galaxy&quot;. Now, Agents J and K must\n" +
-                "stop the bug before it can escape with the galaxy."))
-        peliculas.add(Pelicula("Toy story",R.drawable.toystory,R.drawable.toystoryheader,"Toy Story is about the &#39;secret life of toys&#39; when people are not around. When Buzz\n" +
-                "Lightyear, a space-ranger, takes Woody&#39;s place as Andy&#39;s favorite toy, Woody doesn&#39;t like\n" +
-                "the situation and gets into a fight with Buzz. Accidentaly Buzz falls out the window and\n" +
-                "Woody is accused by all the other toys of having killed him. He has to go out of the house\n" +
-                "to look for him so that they can both return to Andys room. But while on the outside they\n" +
-                "get into all kind of trouble while trying to get home."))
-        peliculas.add(Pelicula("Inception",R.drawable.inception,R.drawable.inceptionheader,"Dom Cobb is a skilled thief, the absolute best in the dangerous art of extraction, stealing\n" +
-                "valuable secrets from deep within the subconscious during the dream state, when the\n" +
-                "mind is at its most vulnerable. Cobb&#39;s rare ability has made him a coveted player in this\n" +
-                "treacherous new world of corporate espionage, but it has also made him an international\n" +
-                "fugitive and cost him everything he has ever loved. Now Cobb is being offered a chance at\n" +
-                "redemption. One last job could give him his life back but only if he can accomplish the\n" +
-                "impossible, inception. Instead of the perfect heist, Cobb and his team of specialists have to\n" +
-                "pull off the reverse: their task is not to steal an idea, but to plant one. If they succeed, it\n" +
-                "could be the perfect crime. But no amount of careful planning or expertise can prepare the\n" +
-                "team for the dangerous enemy that seems to predict their every move. An enemy that only\n" +
-                "Cobb could have seen coming."))
+        peliculas.add(Pelicula("Big hero",R.drawable.bighero6,R.drawable.headerbighero6,"When a devastating event befalls the city of San Fransokyo and catapults Hiro into the midst of danger, he turns to Baymax and his close friends adrenaline junkie Go Go Tomago, neatnik Wasabi, chemistry whiz Honey Lemon and fanboy Fred. Determined to uncover the mystery, Hiro transforms his friends into a band of high-tech heroes called 'Big Hero 6'."))
+        peliculas.add(Pelicula("1917",R.drawable.mil,R.drawable.milheader,"British trenches somewhere in France. World war has been going on for the third year, heroic illusions have dissipated; general mood - boredom and fatigue. Stuff the belly, sleep, return home to Christmas Eve. On another quiet day, when nothing happens, two young soldiers, Blake and Schofield, are summoned to the general, who instructs them to send an important message to Colonel MacKenzie in the Second Devonshire Battalion, whose telephone connection was cut off by the enemy."))
+        peliculas.add(Pelicula("Leap Year",R.drawable.leapyear,R.drawable.leapyearheader,"A woman who has an elaborate scheme to propose to her boyfriend on Leap Day, an Irish tradition which occurs every time the date February 29 rolls around, faces a major setback when bad weather threatens to derail her planned trip to Dublin. With the help of an innkeeper, however, her cross-country odyssey just might result in her getting engaged."))
+        peliculas.add(Pelicula("Men in black",R.drawable.mib,R.drawable.mibheader,"Based off of the comic book. Unbeknownst to other people, there is a private agency codenamed MiB. This agency is some kind of extra terrestrial surveillance corporation. Then,one of the agency finest men only going by the name K (Tommy Lee Jones) , is recruiting for a new addition to the agency. He has chosen James Edwards (Will Smith) of the N.Y.P.D. Then, one day, a flying saucer crashes into Earth. This was an alien a part of the Bug race. He takes the body of a farmer (Vincent D Onofrio) and heads to New York.He is searching for a super energy source called he Galaxy. Now, Agents J and K must stop the bug before it can escape with the galaxy."))
+        peliculas.add(Pelicula("Toy story",R.drawable.toystory,R.drawable.toystoryheader,"Toy Story is about the &#39;secret life of toys&#39; when people are not around. When Buzz Lightyear, a space-ranger, takes Woody&#39;s place as Andy&#39;s favorite toy, Woody doesnt like the situation and gets into a fight with Buzz. Accidentally Buzz falls out the window andWoody is accused by all the other toys of having killed him. He has to go out of the house to look for him so that they can both return to Andys room. But while on the outside they get into all kind of trouble while trying to get home."))
+        peliculas.add(Pelicula("Inception",R.drawable.inception,R.drawable.inceptionheader,"Dom Cobb is a skilled thief, the absolute best in the dangerous art of extraction, stealing valuable secrets from deep within the subconscious during the dream state, when the mind is at its most vulnerable. Cobb&#39;s rare ability has made him a coveted player in this treacherous new world of corporate espionage, but it has also made him an international fugitive and cost him everything he has ever loved. Now Cobb is being offered a chance at redemption. One last job could give him his life back but only if he can accomplish the impossible, inception. Instead of the perfect heist, Cobb and his team of specialists have to pull off the reverse: their task is not to steal an idea, but to plant one. If they succeed, it could be the perfect crime. But no amount of careful planning or expertise can prepare the team for the dangerous enemy that seems to predict their every move. An enemy that onlyCobb could have seen coming."))
 
         peliculas.add(Pelicula("Dr. House",R.drawable.drhouse,R.drawable.househeader,"The series follows the life of anti-social, pain killer addict, witty and arrogant medical doctor Gregory House (Hugh Laurie) with only half a muscle in his right leg. He and his team of medical doctors try to cure complex and rare diseases from very ill ordinary people in the United States of America."))
-        peliculas.add(Pelicula("Dr. Who",R.drawable.drwho,R.drawable.drwhoheader,"Traveling across time and space, the immortal time-lord known as &#39;The Doctor&#39; travels across the\n" +
-                "universe with his many companions and his loyal shape-shifting space-ship: The TARDIS. The\n" +
-                "Doctor faces many threats across many generations: from The Daleks, The Cybermen and his time-\n" +
-                "lord adversary The Master to the sinister Davros, creator of The Daleks."))
-        peliculas.add(Pelicula("Friends",R.drawable.friends,R.drawable.friendsheader,"Rachel Green, Ross Geller, Monica Geller, Joey Tribbiani, Chandler Bing and Phoebe Buffay are\n" +
-                "six 20 something year-olds, living off of one another in the heart of New York City. Over the course\n" +
-                "of ten years, this average group of buddies goes through massive mayhem, family trouble, past and\n" +
-                "future romances, fights, laughs, tears and surprises as they learn what it really means to be a\n" +
-                "friend."))
-        peliculas.add(Pelicula("Smallville",R.drawable.smallville,R.drawable.smallvilleheader,"The numerous miraculous rescues by the local wonder boy Clark have aroused suspicions amongst\n" +
-                "colonials of Smallville. Interestingly, the boy has managed to downplay his acts of various heroic\n" +
-                "egresses in the past. They say he&#39;s either too fast or has a penchant for finding trouble. He was\n" +
-                "found by Martha and Jonathan Kent on the day of the Meteor Shower, and subsequently adopted.\n" +
-                "Clark&#39;s friend Lex Luthor, the only heir of Luthorcorp, has been secretly investigating grounds for\n" +
-                "Clark&#39;s outlandish valor. However, on the face of it, Clark just seems a normal boy who&#39;s slightly\n" +
-                "more secretive than usual."))
+        peliculas.add(Pelicula("Dr. Who",R.drawable.drwho,R.drawable.drwhoheader,"Traveling across time and space, the immortal time-lord known as &#39;The Doctor&#39; travels across the universe with his many companions and his loyal shape-shifting space-ship: The TARDIS. TheDoctor faces many threats across many generations: from The Daleks, The Cybermen and his time-lord adversary The Master to the sinister Davros, creator of The Daleks."))
+        peliculas.add(Pelicula("Friends",R.drawable.friends,R.drawable.friendsheader,"Rachel Green, Ross Geller, Monica Geller, Joey Tribbiani, Chandler Bing and Phoebe Buffay aresix 20 something year-olds, living off of one another in the heart of New York City. Over the course of ten years, this average group of buddies goes through massive mayhem, family trouble, past and future romances, fights, laughs, tears and surprises as they learn what it really means to be a friend."))
+        peliculas.add(Pelicula("Smallville",R.drawable.smallville,R.drawable.smallvilleheader,"The numerous miraculous rescues by the local wonder boy Clark have aroused suspicions amongst colonials of Smallville. Interestingly, the boy has managed to downplay his acts of various heroic egresses in the past. They say he&#39;s either too fast or has a penchant for finding trouble. He was found by Martha and Jonathan Kent on the day of the Meteor Shower, and subsequently adopted Clark friend Lex Luthor, the only heir of Luthorcorp, has been secretly investigating grounds for Clarks outlandish valor. However, on the face of it, Clark just seems a normal boy who&#39;s slightly more secretive than usual."))
 
     }
 
